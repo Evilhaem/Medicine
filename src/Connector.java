460f7a94cab38f2b9027aa1576a20a43c894db5e
evilhaem <evilhaem@gmail.com>
2023-04-12 18:19:37 +0800

    remove unnecessay files

--- src/Connector.java
+++ src/Connector.java
@@ -4,11 +4,13 @@ import java.util.logging.Logger;
 
 public class Connector {
     static Connection instance;
+    static String username = "root";
+    static String password = "B10n24p300859609.@";
 
     public static Connection getInstance() {
         if(instance==null){
             try {
-            instance = DriverManager.getConnection("jdbc:mysql://localhost:3306/simple_medicine?" + "user=root&password=B10n24p300859609.@");
+            instance = DriverManager.getConnection("jdbc:mysql://localhost:3306/simple_medicine?" + "user=" + username + "&password=" + password);
             } catch (SQLException ex) {
                 Logger.getLogger(PillFrame.class.getName()).log(Level.SEVERE, null, ex);
             }
